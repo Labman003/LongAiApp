7016a12f59b4a5cc11f3742ca0b4d933861e4f47
Labman003 <wenbin.private@gmail.com>
2015-07-22 06:25:19 +0800

    test

--- longai/src/main/java/com/ouzhouren/base/BaseApplication.java
+++ longai/src/main/java/com/ouzhouren/base/BaseApplication.java
@@ -2,7 +2,7 @@ package com.ouzhouren.base;
 
 import android.app.Application;
 
-/**
+/**test
  * 做一些所有app都会用到的基础初始化或者配置。之后其他应用的application应该都继承此BaseApplication。
  */
 public class BaseApplication extends Application {
