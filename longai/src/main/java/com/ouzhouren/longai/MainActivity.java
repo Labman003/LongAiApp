1dfff27291879e801c14bb46927e340721f7064b
Labman003 <wenbin.private@gmail.com>
2015-07-30 09:41:43 +0800

    MD化实验

--- longai/src/main/java/com/ouzhouren/longai/MainActivity.java
+++ longai/src/main/java/com/ouzhouren/longai/MainActivity.java
@@ -1,12 +1,25 @@
 package com.ouzhouren.longai;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
@@ -23,14 +36,27 @@ import com.soundcloud.android.crop.Crop;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    Activity mAc;
     SubsamplingScaleImageView resultView;
     ImageView imageView;
+    TextView textView;
+    private CoordinatorLayout rootLayout;
+    private FloatingActionButton fabBtn;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    NavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAc = this;
         resultView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         imageView = (ImageView) findViewById(R.id.imageView2);
+        textView = (TextView) findViewById(R.id.text);
 /************ Initialize ImageLoader with configuration.***********************/
         ImageLoaderConfiguration config = new ImageLoaderConfiguration
                 .Builder(this)
@@ -53,7 +79,57 @@ public class MainActivity extends AppCompatActivity {
         ImageLoader.getInstance().init(config);//全局初始化此配置
         ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
         imageLoader.displayImage("http://img4.douban.com/view/photo/photo/public/p2255230989.jpg",imageView);
+      /*****************选择图片裁剪********************/
       //  Crop.pickImage(this);
+        /*******************MD******************/
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+        fabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rootLayout, "收到彭菊的来信!", Snackbar.LENGTH_SHORT)
+                        .setAction("拆开", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                            }
+                        })
+                        .show();
+            }
+        });
+        /*****************ToolBar*********************/
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        /*********************Tab***********************/
+//        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        /***************Drawer*********************/
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.welcomanim_title_login, R.string.hello_world);
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /*********bar折叠*************/
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbarLayout.setTitle("龙爱私房菜");
+        /******************抽屉导航************************/
+        navigation = (NavigationView) findViewById(R.id.navigation);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.navItem1:
+                        break;
+                    case R.id.navItem2:
+                        break;
+                    case R.id.navItem3:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -63,20 +139,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent result) {
         if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
@@ -99,8 +162,40 @@ public class MainActivity extends AppCompatActivity {
           //  imageView.setImage(ImageSource.asset("map.png"))
 // ... or ...
             resultView.setImage(ImageSource.uri(Crop.getOutput(result)));
+
         } else if (resultCode == Crop.RESULT_ERROR) {
             Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
