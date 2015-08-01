5ca65592f48e39991cfbf3d89e657e02c9d9970a
Labman003 <wenbin.private@gmail.com>
2015-08-01 19:51:33 +0800

    创建Fragment，并与Tab绑定协作

--- longai/src/main/java/com/ouzhouren/longai/MainActivity.java
+++ longai/src/main/java/com/ouzhouren/longai/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ouzhouren.longai;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
@@ -12,6 +11,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,25 +27,22 @@ import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
 import com.soundcloud.android.crop.Crop;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import java.io.File;
-import java.util.Calendar;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    private final int FRAGMENT_SEARCH = 0;
+    private final int FRAGMENT_TALK = 1;
+    private final int FRAGMENT_NEWS = 2;
+    private final int FRAGMENT_EVENT = 3;
+
     Activity mAc;
     SubsamplingScaleImageView resultView;
     ImageView imageView;
@@ -59,41 +56,43 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     CollapsingToolbarLayout collapsingToolbarLayout;
     NavigationView navigation;
     private SwipeRefreshLayout mSwipeRefreshWidget;
-  //  ElasticDownloadView mElasticDownloadView;
+    private ViewPager mViewPager;
+    private MainFragmentStatePagerAdapter pagerAdapter;
+    //  ElasticDownloadView mElasticDownloadView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAc = this;
-        resultView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
-        imageView = (ImageView) findViewById(R.id.imageView2);
-        textView = (TextView) findViewById(R.id.text);
+//        resultView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
+//        imageView = (ImageView) findViewById(R.id.imageView2);
+//        textView = (TextView) findViewById(R.id.text);
 /************ Initialize ImageLoader with configuration.***********************/
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration
-                .Builder(this)
-                .memoryCacheExtraOptions(480, 800) // max width, max height，即保存的每个缓存文件的最大长宽
-              //  .discCacheExtraOptions(480, 800, CompressFormat.JPEG, 75, null) // Can slow ImageLoader, use it carefully (Better don't use it)/设置缓存的详细信息，最好不要设置这个
-                .threadPoolSize(3)//线程池内加载的数量
-                .threadPriority(Thread.NORM_PRIORITY - 2)
-                .denyCacheImageMultipleSizesInMemory()
-                .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024)) // You can pass your own memory cache implementation/你可以通过自己的内存缓存实现
-                .memoryCacheSize(2 * 1024 * 1024)
-                .discCacheSize(50 * 1024 * 1024)
-                .discCacheFileNameGenerator(new Md5FileNameGenerator())//将保存的时候的URI名称用MD5 加密
-                .tasksProcessingOrder(QueueProcessingType.LIFO)
-                .discCacheFileCount(100) //缓存的文件数量
-               // .discCache(new UnlimitedDiscCache(cacheDir))//自定义缓存路径
-                .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
-                .imageDownloader(new BaseImageDownloader(this, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
-                .writeDebugLogs() // Remove for release app
-                .build();//开始构建
-        ImageLoader.getInstance().init(config);//全局初始化此配置
-        ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
-        imageLoader.displayImage("http://img4.douban.com/view/photo/photo/public/p2255230989.jpg",imageView);
+//        ImageLoaderConfiguration config = new ImageLoaderConfiguration
+//                .Builder(this)
+//                .memoryCacheExtraOptions(480, 800) // max width, max height，即保存的每个缓存文件的最大长宽
+//              //  .discCacheExtraOptions(480, 800, CompressFormat.JPEG, 75, null) // Can slow ImageLoader, use it carefully (Better don't use it)/设置缓存的详细信息，最好不要设置这个
+//                .threadPoolSize(3)//线程池内加载的数量
+//                .threadPriority(Thread.NORM_PRIORITY - 2)
+//                .denyCacheImageMultipleSizesInMemory()
+//                .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024)) // You can pass your own memory cache implementation/你可以通过自己的内存缓存实现
+//                .memoryCacheSize(2 * 1024 * 1024)
+//                .discCacheSize(50 * 1024 * 1024)
+//                .discCacheFileNameGenerator(new Md5FileNameGenerator())//将保存的时候的URI名称用MD5 加密
+//                .tasksProcessingOrder(QueueProcessingType.LIFO)
+//                .discCacheFileCount(100) //缓存的文件数量
+//               // .discCache(new UnlimitedDiscCache(cacheDir))//自定义缓存路径
+//                .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+//                .imageDownloader(new BaseImageDownloader(this, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+//                .writeDebugLogs() // Remove for release app
+//                .build();//开始构建
+//        ImageLoader.getInstance().init(config);//全局初始化此配置
+//        ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
+//        imageLoader.displayImage("http://img4.douban.com/view/photo/photo/public/p2255230989.jpg",imageView);
       /*****************选择图片裁剪********************/
       //  Crop.pickImage(this);
-        /*******************MD******************/
+        /*******************FB******************/
         rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
         fabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
         fabBtn.setOnClickListener(new View.OnClickListener() {
@@ -111,12 +110,6 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         /*****************ToolBar*********************/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        /*********************Tab***********************/
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.addTab(tabLayout.newTab().setText("缘分"));
-        tabLayout.addTab(tabLayout.newTab().setText("消息"));
-        tabLayout.addTab(tabLayout.newTab().setText("课室"));
-        tabLayout.addTab(tabLayout.newTab().setText("活动"));
         /***************Drawer*********************/
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.welcomanim_title_login, R.string.hello_world);
@@ -124,8 +117,8 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         /*********bar折叠*************/
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
-        collapsingToolbarLayout.setTitle("龙爱");
+     //   collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+      //  collapsingToolbarLayout.setTitle("龙爱");
         /******************抽屉导航************************/
         navigation = (NavigationView) findViewById(R.id.navigation);
         navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -175,29 +168,56 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 //                })
 //                .show();
         /***************TimePicker****************/
-        Calendar now = Calendar.getInstance();
-//        DatePickerDialog dpd = DatePickerDialog.newInstance(
+//        Calendar now = Calendar.getInstance();
+////        DatePickerDialog dpd = DatePickerDialog.newInstance(
+////                MainActivity.this,
+////                now.get(Calendar.YEAR),
+////                now.get(Calendar.MONTH),
+////                now.get(Calendar.DAY_OF_MONTH)
+////        );
+////        dpd.show(getFragmentManager(), "Datepickerdialog");
+//        TimePickerDialog tpd = TimePickerDialog.newInstance(
 //                MainActivity.this,
-//                now.get(Calendar.YEAR),
-//                now.get(Calendar.MONTH),
-//                now.get(Calendar.DAY_OF_MONTH)
+//                now.get(Calendar.HOUR_OF_DAY),
+//                now.get(Calendar.MINUTE),
+//                false
 //        );
-//        dpd.show(getFragmentManager(), "Datepickerdialog");
-        TimePickerDialog tpd = TimePickerDialog.newInstance(
-                MainActivity.this,
-                now.get(Calendar.HOUR_OF_DAY),
-                now.get(Calendar.MINUTE),
-                false
-        );
-        tpd.setThemeDark(false);
-        tpd.vibrate(false);
-        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialogInterface) {
-                Log.d("TimePicker", "Dialog was cancelled");
-            }
-        });
-        tpd.show(getFragmentManager(), "Timepickerdialog");
+//        tpd.setThemeDark(false);
+//        tpd.vibrate(false);
+//        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+//            @Override
+//            public void onCancel(DialogInterface dialogInterface) {
+//                Log.d("TimePicker", "Dialog was cancelled");
+//            }
+//        });
+//        tpd.show(getFragmentManager(), "Timepickerdialog");
+        /******************* Tab & ViewPager *******/
+        Log.i("调试", "1");
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        // 设置ViewPager的数据等
+        // init view pager
+        Log.i("调试","2");
+        pagerAdapter = new MainFragmentStatePagerAdapter(getSupportFragmentManager());
+        if(pagerAdapter.getItem(0) == null){
+            Log.i("调试","空");
+            Log.i("调试", String.valueOf(pagerAdapter.getCount()));
+        }else{
+            Log.i("调试",pagerAdapter.getItem(0).toString());
+        }
+
+       mViewPager.setAdapter(pagerAdapter);
+        Log.i("调试","3");
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        Log.i("调试","4");
+//        tabLayout.addTab(tabLayout.newTab().setText("缘分"));
+//        tabLayout.addTab(tabLayout.newTab().setText("消息"));
+//        tabLayout.addTab(tabLayout.newTab().setText("课室"));
+//        tabLayout.addTab(tabLayout.newTab().setText("活动"));
+        Log.i("调试","5");
+        tabLayout.setupWithViewPager(mViewPager);
+        Log.i("调试","6");
+    //    tabLayout.setTabsFromPagerAdapter(pagerAdapter);
+        Log.i("调试","7");
     }
 
     @Override
---
+++ longai/src/main/java/com/ouzhouren/longai/MainFragmentStatePagerAdapter.java
@@ -0,0 +1,64 @@
+package com.ouzhouren.longai;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+        private List<Fragment> mFragmentList;
+        private List<String> titleList;
+        private SearchFragment searchFragment;
+        private NewsFragment newsFragment;
+        private TalksFragment talksFragment;
+        private  EventsFragment eventsFragment;
+        public MainFragmentStatePagerAdapter(FragmentManager fm) {
+            super(fm);
+            if(titleList == null){
+                this.titleList = new ArrayList<String>();
+                this.titleList.add("缘分");
+                this.titleList.add("消息");
+                this.titleList.add("课室");
+                this.titleList.add("活动q");
+            }
+            if(mFragmentList == null){
+                this.mFragmentList = new ArrayList<Fragment>();
+                searchFragment =new SearchFragment();
+                this.mFragmentList.add(searchFragment);
+                talksFragment =new TalksFragment();
+                this.mFragmentList.add(talksFragment);
+                newsFragment = new NewsFragment();
+                this.mFragmentList.add(newsFragment);
+                eventsFragment =new EventsFragment();
+                this.mFragmentList.add(eventsFragment);
+            }
+
+        }
+
+        public Fragment getFragement(int position) {
+            if (mFragmentList.get(position) != null) {
+               return mFragmentList.get(position);
+            }
+            return null;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            if (mFragmentList.get(position) != null) {
+                return mFragmentList.get(position);
+            }
+            return null;
+        }
+
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titleList.get(position);
+        }
+    }
