4642e2d8757d0b6f21ba4e106b362209ccc41ea1
Labman003 <wenbin.private@gmail.com>
2015-08-05 15:16:14 +0800

    ImageLoader配置

--- longai/src/main/java/com/ouzhouren/base/BaseApplication.java
+++ longai/src/main/java/com/ouzhouren/base/BaseApplication.java
@@ -2,6 +2,14 @@ package com.ouzhouren.base;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+
 /**test
  * 做一些所有app都会用到的基础初始化或者配置。之后其他应用的application应该都继承此BaseApplication。
  */
@@ -21,6 +29,30 @@ public class BaseApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		instance = this;
+		initConfig();
+	}
+
+	private void initConfig() {
+		/************ Initialize ImageLoader with configuration.***********************/
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration
+				.Builder(this)
+				.memoryCacheExtraOptions(480, 800) // max width, max height，即保存的每个缓存文件的最大长宽
+						//  .discCacheExtraOptions(480, 800, CompressFormat.JPEG, 75, null) // Can slow ImageLoader, use it carefully (Better don't use it)/设置缓存的详细信息，最好不要设置这个
+				.threadPoolSize(3)//线程池内加载的数量
+				.threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()
+				.memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024)) // You can pass your own memory cache implementation/你可以通过自己的内存缓存实现
+				.memoryCacheSize(2 * 1024 * 1024)
+				.discCacheSize(50 * 1024 * 1024)
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())//将保存的时候的URI名称用MD5 加密
+				.tasksProcessingOrder(QueueProcessingType.LIFO)
+				.discCacheFileCount(100) //缓存的文件数量
+						// .discCache(new UnlimitedDiscCache(cacheDir))//自定义缓存路径
+				.defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+				.imageDownloader(new BaseImageDownloader(this, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+				.writeDebugLogs() // Remove for release app
+				.build();//开始构建
+		ImageLoader.getInstance().init(config);//全局初始化此配置
 	}
 
 }
