f81110eb312aca67ddcc2fd684e9225ebe644b9c
郭泽锋 <gzf175@163.com>
2015-08-13 16:08:05 +0800

    mainFragmentStatePagerAdapter

--- longai/src/main/java/com/ouzhouren/longai/MainFragmentStatePagerAdapter.java
+++ longai/src/main/java/com/ouzhouren/longai/MainFragmentStatePagerAdapter.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.ouzhouren.longai.module.news.NewsFragment;
 import com.ouzhouren.longai.module.search.SearchFragment;
 
 import java.util.ArrayList;
