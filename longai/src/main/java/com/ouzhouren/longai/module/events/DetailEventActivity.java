a7dd5ddf8854aa58d6d6f45be50e7475bf1b073c
Labman003 <wenbin.private@gmail.com>
2015-08-19 10:36:35 +0800

    活动详情页界面

--- longai/src/main/java/com/ouzhouren/longai/module/events/DetailEventActivity.java
+++ longai/src/main/java/com/ouzhouren/longai/module/events/DetailEventActivity.java
@@ -1,18 +1,43 @@
 package com.ouzhouren.longai.module.events;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.ouzhouren.longai.R;
 
 public class DetailEventActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_event);
+        webView = (WebView) findViewById(R.id.webview);
+        //设置WebView属性，能够执行Javascript脚本
+        webView.getSettings().setJavaScriptEnabled(false);
+
+        //打开网页时不调用系统浏览器， 而是在本WebView中显示
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        //打开页面时， 自适应屏幕：
+        WebSettings webSettings =   webView .getSettings();
+        webSettings.setUseWideViewPort(true);//设置此属性，可任意比例缩放
+        webSettings.setLoadWithOverviewMode(true);
+
+        //加载需要显示的网页
+        webView.loadUrl("http://www.baidu.com/");
     }
 
     @Override
