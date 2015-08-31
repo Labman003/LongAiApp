c8bac9e45cd17265e0dc9de59d4884726e85ec09
Labman003 <wenbin.private@gmail.com>
2015-08-31 16:04:57 +0800

    用户类型类，活动状态类

---
+++ longai/src/main/java/com/ouzhouren/longai/constant/UserType.java
@@ -0,0 +1,9 @@
+package com.ouzhouren.longai.constant;
+
+/**
+ * Created by BenPC on 2015/8/30.
+ */
+public class UserType {
+    public static String NORMAL = "normal";
+    public static String VIP = "vip";
+}
