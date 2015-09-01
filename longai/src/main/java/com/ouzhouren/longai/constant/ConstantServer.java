ce96205d77da874d6e4dc09b505441fb4b29c847
Labman003 <wenbin.private@gmail.com>
2015-09-01 16:10:59 +0800

    finish login and register function; finish MomentBussiness ;

--- longai/src/main/java/com/ouzhouren/longai/constant/ConstantServer.java
+++ longai/src/main/java/com/ouzhouren/longai/constant/ConstantServer.java
@@ -5,12 +5,22 @@ package com.ouzhouren.longai.constant;
  */
 public class ConstantServer {
     public static String PRE_FIX = "http://";
-    public static String HOSTNAME = "miao-mac.local:8080";
+    //public static String HOSTNAME = "tom:8080";
+    public static String HOSTNAME = "192.168.1.103:8080";
     public static String PATCH_LOGIN = "/longai/user/login";
-    public static String PATCHP_REGISTER = "longai/user/register";
+    public static String PATCHP_REGISTER = "/longai/user/register";
+    //todo
+    public static String PATCH_SEND_MOMENT="/longai/moment/addMoment";
+    public static String PATCH_DELETE_MOMENT="/longai/moment/deleteMoment";
+    public static String PATCH_GET_MOMENT="/longai/moment/getAllMomentVo";
+
+
     public static String PATCH_INSERT_DETAIL = "/longai/user/insertDetail";
     public static String PATCH_USERICON = "/longai/user/userIcon";
     public static String PATCH_GET_DETAIL = "/longai/user/getDetail";
     public static String PATCH_GET_PICS = "/longai/picture/getTotalPic";
     public static String PATCH_PIC_UPLOAD = "/longai/picture/picUpload";
+    //todo
+    public static String PATCH_EVENTS = "/longai/picture/picUpload";
+    public static String PATCH_EVENT_DETAIL = "/longai/picture/picUpload";
 }
